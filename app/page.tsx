97dd3e41c008524b04412ac35c9a1c1ca46ae815
Tanguy <tanguy.folliot@epitech.eu>
2025-09-28 17:13:39 +0200

    feat: add a mouse event

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
-// import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [mousePos, setMousePos] = useState<[number, number]>([0, 0]);
+
+  const mouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const bounds = e.currentTarget.getBoundingClientRect();
+    setMousePos([e.clientX - bounds.left, e.clientY - bounds.top]);
+  }
+
   return (<>
-    <div className="w-full bg-black flex justify-center">
-      <img className="relative w-[90%]" src="/garden.png"/>
+    <div>
+      <div className="w-full bg-black flex justify-center">
+        <div onMouseMove={mouseMove} className="relative w-[90%] overflow-hidden">
+          <p className="absolute bg-blue-200 z-10 duration-100 h-[20px] whitespace-nowrap"
+            style={{
+              left: mousePos[0] + 15,
+              top: mousePos[1] - 20,
+            }}
+            >{mousePos[0].toFixed(0)} {mousePos[1].toFixed(0)}</p>
+          <img className="w-full" src="/garden.png"/>
+        </div>
+      </div>
     </div>
-        {/* <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-          /> */}
     </>);
 }
